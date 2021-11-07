aec8cfd335634ab21a96953ceb01857bdcbca8e3
Christian G <43790095+ChrisGy@users.noreply.github.com>
2021-11-07 20:58:11 +0100

    'Second commit'

--- DataOptimizerApp.py
+++ DataOptimizerApp.py
@@ -1,6 +1,6 @@
 import pandas as pd
 #import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 # import seaborn as sns
 from sklearn.model_selection import train_test_split
 import sklearn.linear_model
@@ -10,12 +10,12 @@ from sklearn.metrics import mean_squared_error
 def  cls(): print("\n"*20)
 # %matplotlib inline: Use plt.show for Python
 
-def bundleClassifier():
+def bundleClassifier(_inputParams):
     pass
 
 # ***************************************
 
-def kpredictor(_input):
+def kpredictor(_inputParams):
     model = sklearn.linear_model.HuberRegressor()
     model.max_iter = 20000
     splitRatio = 0.1
@@ -24,14 +24,15 @@ def kpredictor(_input):
     model = getattr(sklearn.linear_model,_model)()
     model.max_iter = 20000
     model.fit(x_train, y_train)
-    model.predict(_input)
+    model.predict(_inputParams)
     
-def defaultCode():
+# def defaultCode():
+if __name__ == "__mvain__":
     models = dir(sklearn.linear_model)
     rawinputdata = pd.read_csv('data21.csv')
     x = rawinputdata[[i for i in list(rawinputdata.columns) if not (i=='k' or i=='Purchases /month' or i=='Pref (primary)'or i=='Prefer optimization' or i=='Pref (second)' or i=='Unnamed')]]
     y = rawinputdata['k']
-    print("y the input is \n",y)
+    # print("y the input is \n",y)
     models.remove('ElasticNet')
     models.remove('ElasticNetCV')
     models.remove('Hinge')
@@ -54,14 +55,15 @@ def defaultCode():
         for i in range(1):
             
             x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = splitRatio,random_state = 0)
-            # model=LinearRegression()
-            model = getattr(sklearn.linear_model,_model)()
+            model=sklearn.linear_model.LinearRegression()
+            # model = getattr(sklearn.linear_model,_model)()
+            print(model)
             model.max_iter = 20000
             model.fit(x_train, y_train)
-            x_test2 = ['4', '1', '0', '10', '3', '2', '5', '3', '8', '2', '0.006359975']
+            x_test2 = pd.DataFrame(data=['4', '1', '0', '10', '3', '2', '5', '3', '8', '2', '0.006359975'])
             print("test data format:\n",x_test2,"\n of type: \n",type(x_test))
-            
-            y_pred=model.predict(pd.DataFrame([x_test2],None,x.columns))
+            y_pred = model.predict(x_test2)
+            # y_pred=model.predict(x_test,None,x.columns)
             print("prediction: ",y_pred)
             # x = inputdata[]
             all_mses.append(mean_squared_error(y_test,y_pred))
@@ -76,7 +78,7 @@ def defaultCode():
         # model=LinearRegression()
         # model.fit(x_train,y_train)
         # y_pred=model.predict(x_test)
-defaultCode()
+# defaultCode()
 
 def runClassifier():
     pass
@@ -88,7 +90,14 @@ if __name__ == "__main__":
     shouldClassify = ''
     while not (shouldClassify in ['1','2']):
         shouldClassify = input("Enter '1' for existing bundle recommendation and '2' for new bundle generator: \n")
+    #Data Collection:
+    inputParams = {}
+    input_columns = ['WhatsApp', 'YouTube', 'FB', 'Twitter', 'News', 'Research','Coursera/edX', 'Webinar', 'Email', 'High social media', 'Games']
+    for i,v in enumerate(input_columns):
+        inputParams[v] = [float(input("{column:<}:  ".format(column=v)))]
     if (shouldClassify == '1'):
         runClassifier()
     else: #shouldClassify is 2
         newBundleRun()
+    
+    
